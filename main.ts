65414031332925ffe4962f329c3d37fd29d71b38
John Cely <celyj@colpatria.com>
2018-08-30 14:39:10 -0500

    Add info bar to display points and area info

--- main.ts
+++ main.ts
@@ -3,9 +3,11 @@ import { take, tap } from 'rxjs/operators';
 import { Point, Parallelogram, Circle } from './src/domain';
 import { CanvasStore } from './src/service/canvas-store';
 import { Painter } from './src/service/painter';
+import { InfoBar } from './src/service/info-bar';
 
 const store: CanvasStore = new CanvasStore();
 const painter: Painter = new Painter('canvas', store);
+const infoBar: InfoBar = new InfoBar('info-table');
 
 const points: Point[] = [];
 
@@ -15,6 +17,7 @@ painter.onCanvasClicked()
     next: (point: Point) => {
       points.push(point);
       painter.paint(point);
+      infoBar.displayPointInfo(point);
     },
     error: (error) => {
       console.warn(error);
@@ -22,10 +25,15 @@ painter.onCanvasClicked()
     complete: () => {
       const parallelogram: Parallelogram = Parallelogram.givenThreePoints(points[0], points[1], points[2]);
       const circle: Circle = new Circle(parallelogram.centerOfMass, Circle.getRadiusGivenArea(parallelogram.area));
-      
+
       painter.paint(parallelogram.point4);
+      infoBar.displayPointInfo(parallelogram.point4);
+
       painter.paint(parallelogram);
+      infoBar.displayCenterOfMassInfo(parallelogram.centerOfMass);
+      infoBar.displayAreaInfo(parallelogram.area);
+
       painter.paint(circle);
       painter.makePointsSelectable();
-    }
+    },
   });
---
+++ src/service/info-bar.ts
@@ -0,0 +1,40 @@
+import { Point } from '../domain';
+
+export class InfoBar {
+  private table: HTMLElement;
+
+  constructor(tableId: string) {
+    this.table = document.getElementById(tableId) as HTMLElement;
+  }
+
+  displayPointInfo(point: Point): void {
+    this.addRow(`Point ${this.table.children.length + 1}`, point);
+  }
+
+  displayCenterOfMassInfo(centerOfMass: Point): void {
+    this.addRow('Center of mass', centerOfMass);
+  }
+
+  displayAreaInfo(area: number): void {
+    this.addRow('Area', area);
+  }
+
+  private addRow(field: string, value: any): void {
+    const th: HTMLElement = this.createElement('th', field);
+    const td: HTMLElement = this.createElement('td', String(value));
+    const tr: HTMLElement = this.createElementContaining('tr', [th, td]);
+    this.table.appendChild(tr);
+  }
+
+  private createElement(element: string, innerHtml: string): HTMLElement {
+    const htmlElement: HTMLElement = document.createElement(element);
+    htmlElement.innerHTML = innerHtml;
+    return htmlElement;
+  }
+
+  private createElementContaining(element: string, children: HTMLElement[]): HTMLElement {
+    const htmlElement: HTMLElement = document.createElement(element);
+    children.forEach(child => htmlElement.appendChild(child));
+    return htmlElement;
+  }
+}
